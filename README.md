142bad7ece7c7f2c79c0effe1190ea8a80fb86bd
13retonnian <13retonnian@protonmail.com>
2021-12-14 17:55:06 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# SAIT Web Development
+# CPNT 265 Achievements
+# Time Stamp Saver
+## Made by Christopher Barber
+
+## Attributions 
+Tony Grimes and Ashlyn Knox code and classes
+[Rob Kendal's Codepen](https://codepen.io/robkendal/pen/dyYMqMP)
--- js/script.js
+++ js/script.js
@@ -49,23 +49,6 @@ const saveButtonHandler = function() {
   a.click();
     
   URL.revokeObjectURL(a.href);
-
-  // const downloadToFile = (content, filename, contentType) => {
-  //   const a = document.createElement('a');
-  //   const file = new Blob([content], {type: contentType});
-    
-  //   a.href= URL.createObjectURL(file);
-  //   a.download = filename;
-  //   a.click();
-    
-  //   URL.revokeObjectURL(a.href);
-  // };
-  
-  // document.querySelector('#btnSave').addEventListener('click', () => {
-  //   const textArea = document.querySelector('textarea');
-    
-  //   downloadToFile(textArea.value, 'my-new-file.txt', 'text/plain');
-  // });
  
 }
 
